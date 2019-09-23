b89b7f138f948f09eb2d9d14655022708880d0e8
Frank Kuipers <frankkuipers@outlook.com>
2019-09-23 22:36:14 +0200

    Removed credits system

--- routes/Products.js
+++ routes/Products.js
@@ -30,9 +30,6 @@ class Products extends Base {
 
         // allow scanning product
         this.regRoute('post', '/', ['code'], [], true).then(this.postProduct.bind(this));
-
-        // allow unlocking product
-        this.regRoute('put', '/:productId', ['productId', 'action'], [], true).then(this.unlockProduct.bind(this));
     };
 
     /**
@@ -104,32 +101,7 @@ class Products extends Base {
                 self.postReward(request, input, response);
             } else {
                 // verify that product is scanned
-                let product = null;
-                let scanned = products[0];
-                for (let prod of request.user.products) {
-                    if (prod._id === scanned._id) {
-                        product = prod;
-                        break;
-                    }
-                }
-
-                if (product === null) {
-                    response.json({
-                        success: false,
-                        error: 'PRODUCT_NOT_SCANNED',
-                        message: 'Paniek! Je kan geen punten krijgen voor dit product als het niet in je winkelmandje zit!'
-                    });
-                    return;
-                }
-
-                if (!product.bought) {
-                    response.json({
-                        success: false,
-                        error: 'PRODUCT_NOT_UNLOCKED',
-                        message: 'Paniek! Je kan geen punten krijgen voor dit product omdat je hem nog niet unlocked hebt.'
-                    });
-                    return;
-                }
+                let product = self.getProductForUser(products[0], request.user);
 
                 if (product.rewarded) {
                     response.json({
@@ -148,7 +120,7 @@ class Products extends Base {
                     if (error || !originalProduct) {
 
                     } else {
-                        if(originalProduct !== null && originalProduct !== undefined && originalProduct.bonus === true) {
+                        if (originalProduct !== null && originalProduct !== undefined && originalProduct.bonus === true) {
                             bonus = true;
                         }
                     }
@@ -158,7 +130,7 @@ class Products extends Base {
                     request.user.save().then(() => {
                         fcm.sendUpdateProduct(product, request.user);
                         fcm.sendUpdateGroupProperty("points", request.user.points, request.user);
-                        if(bonus) {
+                        if (bonus) {
                             response.json({
                                 success: true,
                                 data: {
@@ -209,14 +181,10 @@ class Products extends Base {
                     var message = "";
                     if (rewards[0].type === 'point') {
                         request.user.points += rewards[0].reward;
-                        message = "Yes! Je hebt er " + rewards[0].reward + " punten bij gekregen. Je hebt nu " + request.user.points + " punten en " + request.user.credits + " actiepunten.";
-                    } else if (rewards[0].type === 'actionpoint') {
-                        request.user.credits += rewards[0].reward;
-                        message = "Yes! Je hebt er " + rewards[0].reward + " actiepunten bij gekregen. Je hebt nu " + request.user.points + " punten en " + request.user.credits + " actiepunten.";
+                        message = "Yes! Je hebt er " + rewards[0].reward + " punten bij gekregen. Je hebt nu " + request.user.points + " punten.";
                     }
                     request.user.save().then(() => {
                         fcm.sendUpdateGroupProperty("points", request.user.points, request.user);
-                        fcm.sendUpdateGroupProperty("credits", request.user.credits, request.user);
                         response.json({
                             success: true,
                             data: {
@@ -245,75 +213,21 @@ class Products extends Base {
 
     /**
      *
-     * @param request
-     * @param input
-     * @param response
+     * @param scannedProduct
+     * @param user
+     * @returns {null}
      */
-    unlockProduct(request, input, response) {
-        if (input.action !== 'unlock') {
-            response.json({
-                success: false,
-                error: 'ACTION_UNKNOWN',
-                message: 'Deze actie bestaat niet.'
-            });
-            return;
-        }
-
-        // verify that product is scanned
+    getProductForUser(scannedProduct, user) {
+        user.addProduct(scannedProduct);
         let product = null;
-        for (let prod of request.user.products) {
-            if (prod._id === input.productId._id) {
+        for (let prod of user.products) {
+            if (prod._id === scannedProduct._id) {
                 product = prod;
                 break;
             }
         }
 
-        if (product === null) {
-            response.json({
-                success: false,
-                error: 'PRODUCT_NOT_SCANNED',
-                message: 'Je kan het product niet unlocken als je het nog niet gescand hebt.'
-            });
-            return;
-        }
-
-        // verify that product is not already unlocked
-        if (product.bought === true) {
-            response.json({
-                success: false,
-                error: 'PRODUCT_ALREADY_UNLOCKED',
-                message: 'Je hebt dit product al unlocked, je hoeft niks te doen.'
-            });
-            return;
-        }
-
-        // verify that group has enough credits
-        if (request.user.credits < product.costs) {
-            response.json({
-                success: false,
-                error: 'NOT_ENOUGH_CREDITS',
-                message: 'Je hebt niet genoeg actiepunten om dit product te unlocken.'
-            });
-            return;
-        }
-
-        // unlock product
-        product.bought = true;
-        request.user.credits = request.user.credits - product.costs;
-        request.user.save().then(() => {
-            fcm.sendUpdateProduct(product, request.user);
-            fcm.sendUpdateGroupProperty("credits", request.user.credits, request.user);
-            response.json({
-                success: true,
-                data: request.user.getAllData()
-            });
-        }).catch(() => {
-            response.json({
-                success: false,
-                error: 'UNKNOWN_ERROR',
-                message: 'Er trad een onbekende fout op.'
-            })
-        });
+        return product;
     }
 }
 
